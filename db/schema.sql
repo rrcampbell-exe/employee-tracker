fcfe0ad09e09b2f250c8682d6c8f32ffbbf17c12
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-08-15 10:26:43 -0500

    Update nomenclature for ids across all tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,24 @@ DROP TABLE IF EXISTS departments;
 USE employee_db;
 
 CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    dept_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    role_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     job_title VARCHAR(50) NOT NULL,
     salary DECIMAL (10,2) NOT NULL,
     department_id INTEGER,
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    FOREIGN KEY (department_id) REFERENCES departments(dept_id)
 );
 
 CREATE TABLE employees (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    employee_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_Name VARCHAR(30) NOT NULL,
     job_title_id INTEGER,
-    FOREIGN KEY (job_title_id) REFERENCES roles(id),
+    FOREIGN KEY (job_title_id) REFERENCES roles(role_id),
     manager_id INTEGER,
-    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id) ON DELETE SET NULL
 );
