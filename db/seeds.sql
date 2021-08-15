ab3c3aca173db230b41218b6cc080e3accb4f1b2
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-08-15 07:23:09 -0500

    Update seeds to list managers first and minimize appearance of NULL values

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,25 +7,26 @@ VALUES
 
 INSERT INTO roles (job_title, salary,  department_id)
 VALUES
-    ('Host', 30000.00, 1),
-    ('Waitstaff', 30000.00, 1),
-    ('Bartender', 30000.00, 1),
-    ('Barback', 30000.00, 1),
-    ('Line Cook', 30000.00, 2),
-    ('Dishwasher', 30000.00, 2),
-    ('Prep Cook', 30000.00, 2),
-    ('Chef', 30000.00, 2),
-    ('Shift Supervisor', 30000.00, 3),
-    ('Assistant Manager', 30000.00, 3),
-    ('General Manager', 30000.00, 3),
-    ('Accountant', 30000.00, 4),
-    ('Marketing Rep', 30000.00, 4);
+    ('Host', 35000.00, 1),
+    ('Waitstaff', 34000.00, 1),
+    ('Bartender', 39000.00, 1),
+    ('Barback', 33000.00, 1),
+    ('Line Cook', 34000.00, 2),
+    ('Dishwasher', 31200.00, 2),
+    ('Prep Cook', 32000.00, 2),
+    ('Chef', 50000.00, 2),
+    ('Shift Supervisor', 41000.00, 3),
+    ('Assistant Manager', 45000.00, 3),
+    ('General Manager', 55000.00, 3),
+    ('Accountant', 45000.00, 4),
+    ('Marketing Rep', 45000.00, 4);
 
 INSERT INTO employees (first_name, last_name, job_title_id, manager_id)
 VALUES
-    ('Helena', 'Padma', 1, NULL),
-    ('Ean', 'Christen', 10, NULL),
     ('Hyrum', 'Harouna', 11, NULL),
+    ('Ean', 'Christen', 10, 1),
+    ('Darina', 'Liliya', 10, 1),
+    ('Helena', 'Padma', 1, 2),
     ('Geraint', 'Gianpiero', 2, 2),
     ('Eógan', 'Neelam', 2, 2),
     ('Raheem', 'Josephus', 2, 2),
@@ -34,16 +35,15 @@ VALUES
     ('Hagit', 'Uno', 3, 2),
     ('Elijah', 'Mel', 4, 2),
     ('Chris', 'Fabijan', 4, 2),
-    ('Urbano', 'Kory', 5, NULL),
-    ('Darina', 'Liliya', 10, 3),
-    ('Jaroslava', 'Zerachiel', 5, 13),
-    ('Murdoch', 'Melvyn', 7, 13),
+    ('Urbano', 'Kory', 5, 3),
+    ('Jaroslava', 'Zerachiel', 5, 3),
+    ('Murdoch', 'Melvyn', 7, 3),
     ('Seisyll', 'Ryôta', 8, 3),
-    ('Gunn', 'Jana', 9, 13),
-    ('Tyrese', 'Elviira', 9, 13),
-    ('Proserpine', 'Zvonko', 9, 2),
+    ('Gunn', 'Jana', 9, 3),
+    ('Tyrese', 'Elviira', 6, 3),
+    ('Proserpine', 'Zvonko', 6, 2),
     ('Ilene', 'Ekewaka', 9, 2),
-    ('Cyril', 'Figgis', 12, 3),
-    ('Hlengiwe', 'Gaylord', 13, 3);
+    ('Cyril', 'Figgis', 12, 1),
+    ('Hlengiwe', 'Gaylord', 13, 1);
 
 
